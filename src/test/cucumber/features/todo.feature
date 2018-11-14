302beba86ca8ca21ab52b234deadf99db978d114
zfareast <de1719@163.com>
2018-11-14 19:04:06 +0800

    add BDD framework Cucumber

---
+++ src/test/cucumber/features/todo.feature
@@ -0,0 +1,5 @@
+Feature: 任务管理
+Scenario: 测试完成任务
+Given 我有3个任务
+When 我完成了1个任务
+Then 我们还剩2个任务
---
+++ src/test/cucumber/step_definitions/TodoStep.java
@@ -0,0 +1,25 @@
+package step_definitions;
+
+import cucumber.api.java.zh_cn.*;
+import cucumber.api.PendingException;
+import static org.junit.Assert.*;
+
+public class TodoStep {
+
+    public void iHaveSomeTasks(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    public void iFinishSomeTasks(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    public void iLeftSomeTasks(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
